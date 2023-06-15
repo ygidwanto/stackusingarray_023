b3db0a74e74a128781217d8ca7dc65bd7d4b405c
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-06-15 10:31:54 +0700

    Menmbah data baru sampai 4 jika lebih maka sudah penuh

--- stackusingarray_023/stackusingarray_023.cpp
+++ stackusingarray_023/stackusingarray_023.cpp
@@ -7,4 +7,18 @@ class Program {
 private:
 	string stack_array[5];
 	int top;
+
+public :
+	Program() {
+		top = -1;
+	}
+
+	string push(string element) {
+		if (top == 4) {
+			cout << "Stack Full" << endl;
+			return"";
+		}
+		top++;
+		stack_array[top] = element;
+	}
 };
